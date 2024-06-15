748474cdbcd8a0e979099df6b196daa6aa0d83ab
cybertron-ant <mediainformationofficer@gmail.com>
2024-06-15 11:53:03 -0400

    initial setup: added NinjaCard stateless widget & set it as 'home' screen in MaterialApp. added empty 'Scaffold' for layout structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
+//  'MaterialApp' widget as the root
+void main() => runApp(MaterialApp(
 
-} // end 'main' function
+  // set 'NinjaCard' widget as the home screen
+  // means 'NinjaCard' will be the first screen shown in the app
+  home: NinjaCard(),
+
+));// end 'main' function
+
+
+// Stateless widgets are immutable & do not have state that changes over time
+class NinjaCard extends StatelessWidget {
+  // Constructor for 'NinjaCard', with a 'key' parameter for widget identification
+  const NinjaCard({super.key});
+
+  // Override 'build' method to describe part of user interface represented by this widget
+  @override
+  Widget build(BuildContext context) {
+
+    // return a Scaffold widget, which is a top-level container for Material design layout
+    return Scaffold();
+
+  }// end 'build' override method
+
+}// end 'NinjaCard' class
