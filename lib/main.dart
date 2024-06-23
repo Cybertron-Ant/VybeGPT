fd3fd71ff37b6fd9c35072489b2aaabdca853624
cybertron-ant <mediainformationofficer@gmail.com>
2024-06-23 15:55:43 -0400

    added Stateless Widget template

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,27 @@ void main() => runApp(MaterialApp(
 
   debugShowCheckedModeBanner: false,
 
-  // set 'NinjaCard' widget as the home screen
-  // means 'NinjaCard' will be the first screen shown in the app
-  home: NinjaCard(),
+  // set 'StatelessTemplate' widget as the home screen
+  // means 'StatelessTemplate' will be the first screen shown in the app
+  home: StatelessTemplate(),
 
 ));// end 'main' function
 
 
 // Stateless widgets are immutable & do not have state that changes over time
-class NinjaCard extends StatelessWidget {
-  // Constructor for 'NinjaCard', with a 'key' parameter for widget identification
-  const NinjaCard({super.key});
+class StatelessTemplate extends StatelessWidget {
+  // Constructor for 'StatelessTemplate', with a 'key' parameter for widget identification
+  const StatelessTemplate({super.key});
 
   // Override 'build' method to describe part of user interface represented by this widget
   @override
   Widget build(BuildContext context) {
 
-    // return a Scaffold widget, which is a top-level container for Material design layout
-    return Scaffold();
+    // return a 'Scaffold' widget, which is a top-level container for Material design layout
+    return Scaffold(
+      body: Center(child: Text("Hello World")),
+    );
 
   }// end 'build' override method
 
-}// end 'NinjaCard' class
+}// end 'StatelessTemplate' class
